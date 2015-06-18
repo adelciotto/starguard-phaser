d344cfaee6a4cd24b6f387d580c2a04e476e84f5
anthonydelciotto <anthdel.developer@gmail.com>
2015-06-19 01:13:23 +1000

    updated static assets paths

--- src/client/states/preload.js
+++ src/client/states/preload.js
@@ -24,19 +24,19 @@ class PreloadState extends State {
         loadingBar.height = 16;
         this.load.setPreloadSprite(loadingBar);
 
-        this.load.spritesheet(Const.SPRITE_SHEET, 'res/img/sheet.png', 12, 12);
-        this.load.spritesheet(Const.GAME_LOGO, 'res/img/logo.png', 24, 12);
-        this.load.image(Const.HTML_LOGO, 'res/img/html.png');
-        this.load.bitmapFont(Const.GAME_FONT, 'res/fonts/plumber_bros.png',
-            'res/fonts/plumber_bros.xml');
+        this.load.spritesheet(Const.SPRITE_SHEET, '/dist/res/img/sheet.png', 12, 12);
+        this.load.spritesheet(Const.GAME_LOGO, '/dist/res/img/logo.png', 24, 12);
+        this.load.image(Const.HTML_LOGO, '/dist/res/img/html.png');
+        this.load.bitmapFont(Const.GAME_FONT, '/dist/res/fonts/plumber_bros.png',
+            '/dist/res/fonts/plumber_bros.xml');
 
-        this.load.tilemap('testmap', 'res/tilemaps/testmap.json',
+        this.load.tilemap('testmap', '/dist/res/tilemaps/testmap.json',
             null, Phaser.Tilemap.TILED_JSON);
-        this.load.image(Const.TILESET_IMG, 'res/tilemaps/tiles.png');
+        this.load.image(Const.TILESET_IMG, '/dist/res/tilemaps/tiles.png');
 
-        this.load.audio(Const.START_SOUND, 'res/sounds/start.mp3');
-        this.load.audio(Const.JUMP_SOUND, 'res/sounds/jump.mp3');
-        this.load.audio(Const.FALL_SOUND, 'res/sounds/hit_ground.mp3');
+        this.load.audio(Const.START_SOUND, '/dist/res/sounds/start.mp3');
+        this.load.audio(Const.JUMP_SOUND, '/dist/res/sounds/jump.mp3');
+        this.load.audio(Const.FALL_SOUND, '/dist/res/sounds/hit_ground.mp3');
     }
 
     create() {
