e16788d38233999244c37ae0f2851ea9a036abd8
anthonydelciotto <anthdel.developer@gmail.com>
2015-06-19 00:01:22 +1000

    removed old db function from index route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -11,20 +11,11 @@ import path from 'path';
 const AppDir = path.dirname(require.main.filename);
 
 export function index(req, res) {
-    // handle db connection
-    handleDatabase(req, res);
-
     // render the index page
     res.sendFile(path.join(AppDir, 'dist', 'index.html'), null, (err) => {
         if (err) {
             req.app.get('log').error(err);
             res.status(err.status).end();
         }
-    });
-}
-
-function handleDatabase(req, res) {
-    req.app.get('storage').getConnection((conn) => {
-        // do some queries
     });
 }
